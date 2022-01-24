5ed10aea3030108484fd3d9e8628a166acb443b2
mykell887 <mykell887@gmail.com>
2022-01-24 09:55:30 +0100

    add cors

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const fileUpload = require('express-fileupload')
 const express = require('express')
 const {PORT, BASE_URL} = process.env
 const {routesArticles, routesUsers, routesVotes} = require('./routes')
+const cors = require('cors')
 
 const app = express()
 
 app.use(fileUpload())
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/uploads', express.static('uploads'))
 
